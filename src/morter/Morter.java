39f0a51fdadc14bfe3008db98479485400c44eb9
lebull <bulldogboy912000@yahoo.com>
2015-04-13 15:29:16 -0500

    Finish up example code
    See ya next time

--- src/morter/Morter.java
+++ src/morter/Morter.java
@@ -9,13 +9,29 @@ import java.io.InputStreamReader;
 //http://examples.javacodegeeks.com/core-java/lang/processbuilder/java-lang-processbuilder-example/
 public class Morter {
 
-	public static void main(String[] args) throws InterruptedException, IOException{
-		
-		ProcessBuilder pb = new ProcessBuilder("echo", "This is a ProcessBuilder Example from JCG");
-		System.out.println("Running echo command");
+	public static void main(String[] args) throws InterruptedException,
+			IOException {
+		ProcessBuilder pb = new ProcessBuilder("ping", "www.google.com", "-n", "3");
+		System.out.println("Run echo command");
 		Process process = pb.start();
-		
-
+		int errCode = process.waitFor();
+		System.out.println("Echo command executed, any errors? " + (errCode == 0 ? "No" : "Yes"));
+		System.out.println("Echo Output:\n" + output(process.getInputStream()));	
+	}
+	
+	private static String output(InputStream inputStream) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new InputStreamReader(inputStream));
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				sb.append(line + System.getProperty("line.separator"));
+			}
+		} finally {
+			br.close();
+		}
+		return sb.toString();
 	}
 
 }
